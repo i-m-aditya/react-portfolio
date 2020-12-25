de7536b33605898788d4f4941af3609b04c223c8
Aditya Pandey <aditya.adityapandey02@gmail.com>
2020-12-25 08:12:46 +0530

    Updates

---
+++ precache-manifest.87c3406c4521922af5ecc9d423601741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8e73c8945c8643fd9796cbde66915d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d0e2234008995c1fe24e",
+    "url": "/react-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "674afc521dbf11a78645",
+    "url": "/react-portfolio/static/js/2.4c30cdca.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-portfolio/static/js/2.4c30cdca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e2234008995c1fe24e",
+    "url": "/react-portfolio/static/js/main.16950255.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
